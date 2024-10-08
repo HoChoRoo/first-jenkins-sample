3832a27e6762e5876a6b32e8fcf8c918c0768b94
HoChoRoo <rttkhc0304@gmail.com>
2024-10-08 16:48:39 +0900

    Ni Hao My World

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -13,6 +13,6 @@ public class HelloController {
     @GetMapping
     public String sayHello() {
         System.out.println("hello!!");
-        return "Ni Hao, World!";
+        return "Ni Hao, My World!";
     }
 }

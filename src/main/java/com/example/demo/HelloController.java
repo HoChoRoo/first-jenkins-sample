ba0c12bcc7c8f5cf348954a107752dabf43f88f6
HoChoRoo <rttkhc0304@gmail.com>
2024-10-08 16:57:11 +0900

    Git Webhook

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -13,6 +13,6 @@ public class HelloController {
     @GetMapping
     public String sayHello() {
         System.out.println("hello!!");
-        return "Ni Hao, My World!";
+        return "Ni Hao, My Chinese World!";
     }
 }
